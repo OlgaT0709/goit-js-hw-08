d6f923d7d435ccec0a44829446e762f28802e1fe
Olga Tolstykhina <ovt0709@gmail.com>
2023-03-01 19:09:19 +0200

    Task 1 and 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createGalary(galleryItems))
+
+function createGalary(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+            `;
+    }).join('');
+};
+
+// створюємо модалку і передаємо велику картинку 
+lightbox = new SimpleLightbox('.gallery a');
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
 
+// створюємо плеєр Vimeo 
+const player = new Vimeo(iframe, {
+  id: 236203659,
+  width: 640,
+  height: 360,
+});
+
+// додаємо обробник події 'timeupdate' до плеєра Vimeo
+player.on('timeupdate', function(data) {
+  // зберігаємо поточний час відтворення у localStorage
+    localStorage.setItem('vimeoPlayerCurrentTime', data.seconds);
+   
+});
+
+// встановлюємо час відтворення з localStorage, якщо він збережений
+const savedTime = localStorage.getItem('vimeoPlayerCurrentTime');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
