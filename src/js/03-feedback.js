babdbacf5af87bcb6487ce1a08e075f07ebf986c
Olga Tolstykhina <ovt0709@gmail.com>
2023-03-02 22:14:27 +0200

    task03 final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,50 +8,50 @@ const LOCALSTORAGE_KEY = "feedback-form-state";
        
 // Під час завантаження сторінки перевіряємо стан сховища, і якщо там є збережені дані, заповнюємо ними поля форми.
 const savedInput = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-if (savedInput !== null) {
+if (savedInput) {
     
     emailRef.value = savedInput.email;
     messageRef.value = savedInput.message;
 }
 
-// Відстежуємо на формі подію input, і щоразу записуємо у локальне сховище об'єкт з полями email і message, 
+// Відстежуємо на формі подію input,  
 // Сховище оновлюється не частіше, ніж раз на 500 мілісекунд (використовуємо бібліотеку lodash.throttle)
 
 formRef.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
-    const form = event.target.form;
 
-    if (form) {
-        const { email, message } = form.elements;
-
+        const { email, message } = event.target.form.elements;
         const formData = {
             email: email.value,
             message: message.value,
         };
+        // і щоразу записуємо у локальне сховище об'єкт з полями email і message,
         localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
-    }
+    
 };
 
-// Під час сабміту форми виводимо у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// Під час сабміту форми 
 formRef.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
     event.preventDefault();
 
     const { email, message } = event.currentTarget.elements;
-    
-    const formData = {
-        email: email.value,
-        message: message.value,
+    // перевіряємо , що поля заповнені 
+    if (email.value !== '' && message.value !== '') {
+        const formData = {
+            email: email.value,
+            message: message.value,
         };
+        // виводимо у консоль об'єкт з полями email, message та їхніми поточними значеннями.
         console.log(formData);
 
          
-    // очищуємо сховище і поля форми
-    event.currentTarget.reset();
-    localStorage.removeItem(LOCALSTORAGE_KEY);
+        // очищуємо сховище і поля форми
+        event.currentTarget.reset();
+        localStorage.removeItem(LOCALSTORAGE_KEY);
     };
- 
+};
     
-           
+
